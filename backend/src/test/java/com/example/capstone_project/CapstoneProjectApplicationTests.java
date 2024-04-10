fe9c01125f29e0a2d9c667e500f9cc46d0bd467e
Tadiwanashe Dzvoti <tadiwapp365@hotmail.com>
2024-04-10 15:00:14 +0100

    Added Spring Boot generated files.

---
+++ backend/src/test/java/com/example/capstone_project/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
