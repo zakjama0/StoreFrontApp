691dfc6b50fa020b0f72d366a8b4af258616db4e
Marvellous Akib <marvellous.akib@gmail.com>
2024-04-10 15:51:25 +0100

    edited model

--- backend/src/main/java/com/example/capstone_project/models/Item.java
+++ backend/src/main/java/com/example/capstone_project/models/Item.java
@@ -1,4 +1,22 @@
 package com.example.capstone_project.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String picture;
+
+    @Column
+    private Stock stock
+
 }
