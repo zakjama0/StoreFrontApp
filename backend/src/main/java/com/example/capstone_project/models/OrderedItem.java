dfd2f21d0848e069fa4f641763201bfa72cd339a
Tadiwanashe Dzvoti <tadiwapp365@hotmail.com>
2024-04-11 09:26:40 +0100

    Created one to many relationship for order items.

--- backend/src/main/java/com/example/capstone_project/models/OrderedItem.java
+++ backend/src/main/java/com/example/capstone_project/models/OrderedItem.java
@@ -3,7 +3,7 @@ package com.example.capstone_project.models;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="ordereditem")
+@Table(name="ordered_item")
 public class OrderedItem {
 
     @Id
@@ -11,6 +11,36 @@ public class OrderedItem {
     @Column
     private long id;
 
+    @OneToMany(mappedBy = "orders")
+    private long orderId;
 
+    @OneToMany(mappedBy = "items")
+    private long itemId;
 
+    public OrderedItem() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+
+    public long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(long itemId) {
+        this.itemId = itemId;
+    }
 }
