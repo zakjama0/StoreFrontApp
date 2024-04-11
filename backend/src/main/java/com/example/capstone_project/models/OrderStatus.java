8447938be2b1cd3f3c2030636ca722ca420f6500
BirindarK <birindar.bahia01@gmail.com>
2024-04-11 11:18:28 +0100

    Completed Order Status and Category Enum

--- backend/src/main/java/com/example/capstone_project/models/OrderStatus.java
+++ backend/src/main/java/com/example/capstone_project/models/OrderStatus.java
@@ -1,10 +1,16 @@
 package com.example.capstone_project.models;
 
 public enum OrderStatus {
-    PENDING,
-    IN_PROGRESS,
-    OUT_FOR_DELIVERY,
-    DELIVERED;
 
-    OrderStatus(){};
+    PENDING("Pending"),
+    IN_PROGRESS("In Progress"),
+    OUT_FOR_DELIVERY("Out for delivery"),
+    DELIVERED("Delivered");
+
+    final String displayStatus;
+
+    OrderStatus(String displayStatus) {
+        this.displayStatus = displayStatus;
+    }
+
 }
