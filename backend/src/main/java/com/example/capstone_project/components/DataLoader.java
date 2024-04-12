87afb9e512cad14c93b19773dd1b140c0e74ab11
Tadiwanashe Dzvoti <tadiwapp365@hotmail.com>
2024-04-12 16:45:14 +0100

    Added Yabbi's item to the dataloader

--- backend/src/main/java/com/example/capstone_project/components/DataLoader.java
+++ backend/src/main/java/com/example/capstone_project/components/DataLoader.java
@@ -48,23 +48,26 @@ public class DataLoader implements ApplicationRunner {
         Customer customer3 = new Customer("Zakaria", "zakaria@schrutefarms.com");
         Customer customer4 = new Customer("Birindar", "birindar@schrutefarms.com");
         Customer customer5 = new Customer("Tadiwanashe", "tadiwanashe@schrutefarms.com");
+        Customer customer6 = new Customer("Yabbi", "Yabbi@schrutefarms.com");
 
         customerRepository.save(customer1);
         customerRepository.save(customer2);
         customerRepository.save(customer3);
         customerRepository.save(customer4);
         customerRepository.save(customer5);
+        customerRepository.save(customer6);
 
-        Item item1 = new Item("Tadiwa's Cup", "https://m.media-amazon.com/images/I/61QE8zyCMPL.jpg", Category.HOME_AND_KITCHEN, 50, 300, "A cup");
-        Item item2 = new Item("Zak's Iron", "https://www.reliancedigital.in/wp-content/uploads/2018/07/Dry-irons.jpg", Category.HOME_AND_KITCHEN, 25, 20000, "An iron");
-        Item item3 = new Item("Saba's Electric Scooter", "https://i.ebayimg.com/images/g/TqgAAOSwluJlMPuk/s-l1200.webp", Category.ELECTRONICS, 10, 27900, "Saba's height adjusted electric scooter");
-        Item item4 = new Item("Marvellous' Eyeliner", "https://picture.drhauschka.co.uk/media/image/be/ed/76/3134663-liquid-eyeliner-container-01-01-420005981.jpg", Category.BEAUTY_AND_WELLNESS, 100, 1299, "Marvellous' eyeliner");
+        Item item1 = new Item("Tadiwa's Cup", "https://m.media-amazon.com/images/I/61QE8zyCMPL.jpg", Category.HOME_AND_KITCHEN, 50, 300, "A cup.");
+        Item item2 = new Item("Zak's Iron", "https://www.reliancedigital.in/wp-content/uploads/2018/07/Dry-irons.jpg", Category.HOME_AND_KITCHEN, 25, 20000, "An iron.");
+        Item item3 = new Item("Saba's Electric Scooter", "https://i.ebayimg.com/images/g/TqgAAOSwluJlMPuk/s-l1200.webp", Category.ELECTRONICS, 10, 27900, "Saba's height adjusted electric scooter.");
+        Item item4 = new Item("Marvellous' Eyeliner", "https://picture.drhauschka.co.uk/media/image/be/ed/76/3134663-liquid-eyeliner-container-01-01-420005981.jpg", Category.BEAUTY_AND_WELLNESS, 100, 1299, "Marvellous' eyeliner.");
         Item item5 = new Item("Surrounded By Idiots", "https://res.cloudinary.com/pippa/image/fetch/h_750,w_750,f_auto/https://assets.pippa.io/shows/6176c02d8283c6a671a7218a/1652182907840-e24d2e84efececbb30ebf4621d6ddc94.jpeg", Category.BOOKS, 1, 999, "Book that described Birindar's capstone project experience.");
-        Item item6 = new Item("Tadiwa's Wig", "https://m.media-amazon.com/images/I/61oeNBfFHBL._AC_UF1000,1000_QL80_.jpg", Category.BEAUTY_AND_WELLNESS, 50, 999, "Wigs that will make you look beautiful");
+        Item item6 = new Item("Tadiwa's Wig", "https://m.media-amazon.com/images/I/61oeNBfFHBL._AC_UF1000,1000_QL80_.jpg", Category.BEAUTY_AND_WELLNESS, 50, 999, "Wigs that will make you look beautiful.");
         Item item7 = new Item("Time of my Life", "https://m.media-amazon.com/images/I/711H10fgD+L._AC_UF894,1000_QL80_.jpg", Category.BOOKS, 1, 1229, "Book that described Saba's capstone project experience.");
-        Item item8 = new Item("Birindar's second screen", "https://cdn.thewirecutter.com/wp-content/media/2021/05/27-inch-monitor-2048px-1572.jpg", Category.ELECTRONICS, 1, 9999, "To help with your programming problems");
-        Item item9 = new Item("Zakaria's Leather Jacket", "https://hidepark.co.uk/cdn/shop/products/Tate-Black-Front.jpg?v=1657043284", Category.FASHION, 1, 799, "Ready for NY fashion week");
-        Item item10 = new Item("EA Sport FC 25", "https://www.fifplay.com/img/public/fc-25.jpg", Category.VIDEO_GAMES, 1, 3999, "No different from the past year");
+        Item item8 = new Item("Birindar's second screen", "https://cdn.thewirecutter.com/wp-content/media/2021/05/27-inch-monitor-2048px-1572.jpg", Category.ELECTRONICS, 1, 9999, "To help with your programming problems.");
+        Item item9 = new Item("Zakaria's Leather Jacket", "https://hidepark.co.uk/cdn/shop/products/Tate-Black-Front.jpg?v=1657043284", Category.FASHION, 1, 799, "Ready for NY fashion week.");
+        Item item10 = new Item("EA Sport FC 25", "https://www.fifplay.com/img/public/fc-25.jpg", Category.VIDEO_GAMES, 1, 3999, "No different from the past year.");
+        Item item11 = new Item("Yabbi's Standing Desk", "https://cdn.shopify.com/s/files/1/2710/8782/files/3a3ec3dc-9b59-47ca-b129-cb75ccf1ae35_2218d3a5-a28a-4e28-a0b4-93a2fefa7a62.jpg", Category.ELECTRONICS, 1, 34900, "Just a standing desk.");
 
         itemRepository.save(item1);
         itemRepository.save(item2);
@@ -76,9 +79,9 @@ public class DataLoader implements ApplicationRunner {
         itemRepository.save(item8);
         itemRepository.save(item9);
         itemRepository.save(item10);
+        itemRepository.save(item11);
 
 
-        Item item11 = new Item("Smartphone Holder", "https://m.media-amazon.com/images/I/61tyIG+b4+L._AC_UF1000,1000_QL80_.jpg", Category.ELECTRONICS, 50, 499, "Convenient holder for smartphones");
         Item item12 = new Item("Elegant Necktie", "https://www.brucleshop.com/11150-large_default/elegant-necktie-in-jacquard-silk-bronze-pattern.jpg", Category.FASHION, 100, 799, "Stylish necktie for formal occasions");
         Item item13 = new Item("Yoga Mat", "https://m.media-amazon.com/images/I/71TDyKXF27L.jpg", Category.BEAUTY_AND_WELLNESS, 30, 999, "High-quality mat for yoga enthusiasts");
         Item item14 = new Item("Stainless Steel Water Bottle", "https://gbsnowsport.com/wp-content/uploads/2023/10/stainless-steel-water-bottle-white-17-oz-back-653795e806839.jpg", Category.HOME_AND_KITCHEN, 80, 299, "Reusable bottle for staying hydrated on the go");
@@ -98,7 +101,7 @@ public class DataLoader implements ApplicationRunner {
         Item item28 = new Item("Denim Jeans", "https://d1b5h9psu9yexj.cloudfront.net/45085/Levi---s-Premium-511-Slim-Fit-Men---s-Jeans_20210518-160540_full.jpeg", Category.FASHION, 100, 1299, "Classic denim jeans for everyday wear");
         Item item29 = new Item("Air Purifier", "https://media.4rgos.it/i/Argos/9549517_R_Z001A?w=750&h=440&qlt=70", Category.HOME_AND_KITCHEN, 20, 4999, "Purify the air in your home with this advanced air purifier");
         Item item30 = new Item("Fitness Watch", "https://www.apple.com/newsroom/images/product/watch/standard/Apple_watch-series7_hero_09142021_big.jpg.large.jpg", Category.BEAUTY_AND_WELLNESS, 30, 2499, "Smartwatch with fitness tracking features to monitor your health");
-        itemRepository.saveAll(Arrays.asList(item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25, item26, item27, item28, item29, item30));
+        itemRepository.saveAll(Arrays.asList(item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25, item26, item27, item28, item29, item30));
 
         Review review1 = new Review(4, "Great cup.", customer2, item1);
         Review review2 = new Review(4, "Amazing Wig, feels really authentic", customer1, item6);
