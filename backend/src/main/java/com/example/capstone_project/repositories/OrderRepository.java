b5a9c7542a0f1afc353f9508516c9812811d152a
Marvellous Akib <marvellous.akib@gmail.com>
2024-04-12 14:15:02 +0100

    added query text

--- backend/src/main/java/com/example/capstone_project/repositories/OrderRepository.java
+++ backend/src/main/java/com/example/capstone_project/repositories/OrderRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("SELECT * FROM orders o WHERE o.customer_id = :customerId")
+    @Query("SELECT o FROM Order o WHERE o.customer.id = :customerId")
     List<Order> findOrdersByCustomerId(Long customerId);
 }
