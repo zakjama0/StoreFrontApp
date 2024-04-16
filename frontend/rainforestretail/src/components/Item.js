6689a5120d8ea8197ba72dc406afa00e9ada356a
Tadiwanashe Dzvoti <tadiwapp365@hotmail.com>
2024-04-16 15:27:44 +0100

    work

--- frontend/rainforestretail/src/components/Item.js
+++ frontend/rainforestretail/src/components/Item.js
@@ -29,7 +29,7 @@ const Item = ({ deleteReview, patchReview, postReview }) => {
                     
                     <div className='review-form'>
                         <h3>Submit a review</h3>
-                        <ReviewForm onSubmit={postReview} />
+                        <ReviewForm postReview={postReview} />
                     </div>
 
                 </div>
