d429bd12d44959c5355d83631b054e98fee00812
Saba Kia <skia6589@gmail.com>
2024-04-17 11:07:17 +0100

    basket stuff

--- frontend/rainforestretail/src/components/OrderList.js
+++ frontend/rainforestretail/src/components/OrderList.js
@@ -1,4 +1,11 @@
+import Order from "./Order";
+
 const OrderList = ({ basketList }) => {
+
+    const basketComponents = basketList.map(basketItem => {
+        return <Order key={basketItem.itemId}/>
+    })
+
     return (
         <>
         
